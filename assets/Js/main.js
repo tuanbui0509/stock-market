d9a41f08062eacdcf051137238ecb2885386ebfa
tuanbui0509 <tuanbui0509@gmail.com>
2021-06-03 20:44:17 +0700

    update completed lightning

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,70 +1,42 @@
-const standard = document.getElementById('AAAstandard')
-const ceil = document.getElementById('AAAceil')
-const floor = document.getElementById('AAAfloor')
+// ceil,tc,floor,lower,higher
+const colors = ['rgb(255, 37, 255)', 'rgb(255, 217, 0)',
+    'rgb(30, 238, 238)', 'rgb(255, 0, 23)', 'rgb(11, 223, 57)']
 
-const bP3 = document.getElementById('AAAbP3')
-const bV3 = document.getElementById('AAAbV3')
-const bP2 = document.getElementById('AAAbP2')
-const bV2 = document.getElementById('AAAbV2')
-const bP1 = document.getElementById('AAAbP1')
-const bV1 = document.getElementById('AAAbV1')
-
-const oP3 = document.getElementById('AAAoP3')
-const oV3 = document.getElementById('AAAoV3')
-const oP2 = document.getElementById('AAAoP2')
-const oV2 = document.getElementById('AAAoV2')
-const oP1 = document.getElementById('AAAoP1')
-const oV1 = document.getElementById('AAAoV1')
 
 
 function changeColor(numStock, tc, floor, ceil) {
-    if (parseFloat(numStock) === parseFloat(ceil)) {
-        return '#ff25ff';
-    }
-    if (parseFloat(numStock) === parseFloat(tc)) {
-        return '#ffd900';
-    }
-    if (parseFloat(numStock) === parseFloat(floor)) {
-        return '#1eeeee';
-    }
-    if (parseFloat(numStock) < parseFloat(tc)) {
-        return '#ff3737';
-    }
-    if (parseFloat(numStock) > parseFloat(tc)) {
-        return '#0f0';
-    }
-
+    if (parseFloat(numStock) === parseFloat(ceil))
+        return colors[0];
+    else if (parseFloat(numStock) === parseFloat(tc))
+        return colors[1];
+    else if (parseFloat(numStock) === parseFloat(floor))
+        return colors[2];
+    else if (parseFloat(numStock) < parseFloat(tc))
+        return colors[3];
+    else if (parseFloat(numStock) > parseFloat(tc))
+        return colors[4];
 }
 
 
-var stocks = document.getElementsByClassName('line-stock');
+let stocks = document.querySelectorAll('.stock');
 
 
 for (let i = 0; i < stocks.length; i++) {
     let line_stock = stocks[i].getElementsByTagName('td');
     let j = 0;
     for (; j < line_stock.length; j++) {
-        console.log(j)
-        let numStock = line_stock[j].getElementsByTagName('span')[0].innerHTML;
-        let tc = line_stock[1].getElementsByTagName('span')[0].innerHTML;
-        let ceil = line_stock[2].getElementsByTagName('span')[0].innerHTML;
-        let floor = line_stock[3].getElementsByTagName('span')[0].innerHTML;
-        if (j === 5 || j === 7 || j === 9 || j === 11 || j === 14 || j === 16 || j === 18) {
-            console.log('curr: ', numStock)
-            console.log('TC: ', tc)
-            console.log('Ceil: ', ceil)
-            console.log('Floor: ', floor)
+        let numStock = line_stock[j].innerHTML;
+        let tc = line_stock[3].innerHTML;
+        let ceil = line_stock[2].innerHTML;
+        let floor = line_stock[1].innerHTML;
+        if (j === 4 || j === 6 || j === 8 || j === 10 || j === 12 || j === 14 || j === 16) {
             let strColor = changeColor(numStock, tc, floor, ceil);
-            console.log(strColor)
-            line_stock[j].getElementsByTagName('span')[0].style.color = strColor;
-            line_stock[j + 1].getElementsByTagName('span')[0].style.color = strColor;
-
+            line_stock[j].style.color = strColor;
+            line_stock[j + 1].style.color = strColor;
+            if (j === 10) line_stock[0].style.color = strColor;
         }
     }
 }
 
-
-
-showDatLenh=() =>{
-    
-}
+let showLanguage = document.querySelector('.header__right-label-language');
+console.log(showLanguage)
